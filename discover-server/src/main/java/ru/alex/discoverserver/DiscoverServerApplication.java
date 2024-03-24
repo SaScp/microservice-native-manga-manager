f3432fddc32981481a22354e89c9f4597d34be5d
Alexander Vorobev <3048453@gmail.com>
2024-03-25 00:14:41 +0300

    Initial commit

---
+++ discover-server/src/main/java/ru/alex/discoverserver/DiscoverServerApplication.java
@@ -0,0 +1,15 @@
+package ru.alex.discoverserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoverServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoverServerApplication.class, args);
+    }
+
+}
---
+++ discover-server/src/test/java/ru/alex/discoverserver/DiscoverServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alex.discoverserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
