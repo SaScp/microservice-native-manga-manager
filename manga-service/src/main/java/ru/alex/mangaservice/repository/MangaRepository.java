1a14c99fe5e307bddca57b4b1428f9573c2f382b
Alexander Vorobev <3048453@gmail.com>
2024-03-28 23:34:39 +0300

    add Jpa in Manga Service

--- manga-service/src/main/java/ru/alex/mangaservice/repository/MangaRepository.java
+++ manga-service/src/main/java/ru/alex/mangaservice/repository/MangaRepository.java
@@ -1,4 +1,9 @@
 package ru.alex.mangaservice.repository;
 
-public class MangaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.alex.mangaservice.model.Manga;
+
+@Repository
+public interface MangaRepository extends JpaRepository<Manga, String> {
 }
